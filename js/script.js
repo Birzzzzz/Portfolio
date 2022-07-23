79a68cfc5750cbd33749ec4fc0a4124000defe2e
Ugo.B <85753147+Birzzzzz@users.noreply.github.com>
2022-07-23 15:05:03 +0200

    style switch

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing",{
+    strings:["web designer","web developer","graphic designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
